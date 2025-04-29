7a933ad489aee80e923b5f111355205f8951c6d8
Ngoy <dngoyb@gmail.com>
2025-04-29 20:48:45 +0200

    chore: configure Docker Compose with environment variables and secure .env handling

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+
+// Load environment variables from .env file
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
