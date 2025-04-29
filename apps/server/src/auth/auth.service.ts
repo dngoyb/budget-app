9e1d19902d5f34fececdbc77227855fe6cbd2afc
Ngoy <dngoyb@gmail.com>
2025-04-29 18:03:15 +0200

    feat(auth): hash user password during registration

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
-  register(createUserDto: CreateUserDto) {
+  async register(createUserDto: CreateUserDto) {
+    const { email, username, password } = createUserDto;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = {
+      email,
+      username,
+      password: hashedPassword,
+    };
     // Logic for user registration (dummy for now)
-    return { message: 'User registered successfully', user: createUserDto };
+    return { message: 'User registered successfully', user };
   }
 
   login(createUserDto: CreateUserDto) {
